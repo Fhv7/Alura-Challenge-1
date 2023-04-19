cc366190207762b2b2b9629dc338a830d291dd8a
Fabián Henry <fhenrynk@gmail.com>
2023-04-18 22:33:32 -0400

    Refactoring + Better General Responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Box, Text, Textarea, Center, VStack } from '@chakra-ui/react'
+import { Button, Card, Box, Text, Textarea, VStack, HStack } from '@chakra-ui/react'
 import './App.css'
 import { Grid, GridItem } from '@chakra-ui/react'
 import { Heading } from '@chakra-ui/react'
@@ -7,18 +7,37 @@ function App() {
   // TODO: Create a function and a stateful variable to handle the input from the Textarea component
   // TODO: Create a function that prints to console the encrypted or decrypted message the user input
   return (
-      <Box ms={32} me={{xl: "8rem", base: "3rem"}} my={16}>
+      <Box>
         <Grid
-            templateRows='auto 1fr auto'
-            templateColumns='repeat(6, 1fr)'
-            gap={4}
+            templateColumns={{lg: 'repeat(6, 1fr)'}}
+            gap={12}
+            mx={{md: 32, sm: 14}}
+            my={16}
             >
-            {/* Heading */}
-            <GridItem colSpan={4}>
-              <Heading as="h1" size="2xl">Encriptador de Texto</Heading>
+            {/* Heading, Input and Buttons */}
+            <GridItem 
+              colSpan={{xl: 4, base: 6}} 
+              me={{xl: 48}}
+            >
+              <Heading as="h1" size="2xl" mb={12}>
+                  Encriptador de Texto
+              </Heading>
+              <VStack>
+              <Textarea 
+                placeholder="Ingresa el texto aquí!" 
+                variant="unstyled" 
+                resize="none"
+                fontSize="3xl"
+                height="58vh"
+                />
+                <HStack>
+                <Button colorScheme="blue" size="lg">Encriptar</Button>
+                <Button colorScheme="blue" size="lg">Desencriptar</Button>
+                </HStack>
+              </VStack>
             </GridItem>
             {/* Encryption / Decryption result */}
-            <GridItem rowSpan={3} colSpan={2}>
+            <GridItem colSpan={{xl: 2, base: 6}}>
               <Card minH="80vh" variant="elevated">
                 <VStack m="auto">
                   <Text fontSize="3xl">
@@ -30,25 +49,6 @@ function App() {
                 </VStack>
               </Card>
             </GridItem>
-            {/* Text Input */}
-            <GridItem colSpan={4} ms={10} me={32} my={10} >
-              <Textarea 
-                placeholder="Ingresa el texto aquí!" 
-                variant="unstyled" 
-                resize="none"
-                fontSize="3xl"
-                height="58vh"
-                />
-            </GridItem>
-            {/* Encrypt Button */}
-            <GridItem colSpan={2} textAlign="center">
-              <Button colorScheme="blue" size="lg">Encriptar</Button>
-            </GridItem>
-            {/* Decrypt Button */}
-            <GridItem colSpan={2} >
-              <Button colorScheme="blue" size="lg">Desencriptar</Button>
-            </GridItem>
-
         </Grid>
       </Box>
     )
