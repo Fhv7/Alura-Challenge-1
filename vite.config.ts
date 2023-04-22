6ead847184d3c9a8f2b9b141580dcfc63aa1b889
Fhv7 <fabian.henry.vilaxa@alumnos.uta.cl>
2023-04-21 23:08:18 -0400

    Homepage

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Alura-Challenge-1-Encrypter",
+  base: "/ChallengeONE-Alura-Encrypter",
   plugins: [react()],
 })
